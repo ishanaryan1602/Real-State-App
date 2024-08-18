599d5e70aaa16296265656a29ef0f40919093492
ishanaryan1602 <github1602@gmail.com>
2024-08-19 00:27:33 +0530

    Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import {BroweserRouter,Routes,Route} from 'react-router-dom'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 export default function App() {
   return (
-    <BroweserRouter>
+    <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
@@ -16,6 +18,6 @@ export default function App() {
       <Route path="/about" element={<About />} />
       <Route path="/profile" element={<Profile />} />
     </Routes>
-    </BroweserRouter>
+    </BrowserRouter>
   )
 }
