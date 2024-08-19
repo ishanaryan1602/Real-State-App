96fd7e2ced0dd52219ca6e56f3e9b261f17c00c3
ishanaryan1602 <github1602@gmail.com>
2024-08-19 14:26:06 +0530

    database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => console.log('connected to db')).catch((err) => console.log(err.message))
 
 const app = express();
 
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log('listening on port 3000');
 });
+  
