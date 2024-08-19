902302ebe14fb3efe75ce7b9eb7b45697ca52611
ishanaryan1602 <github1602@gmail.com>
2024-08-19 15:41:40 +0530

    test api router contollers

--- api/index.js
+++ api/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRouter from './routes/user.route.js';
 dotenv.config();
-
 mongoose.connect(process.env.MONGO).then(() => console.log('connected to db')).catch((err) => console.log(err.message))
-
 const app = express();
+
+app.get('/test', (req, res) => {
+    res.send('Hello World');
+});
+
+app.use("/api/user",userRouter);
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
