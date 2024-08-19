51b8fe9a62212e96f9bfedce4a5320bd27dc66d7
ishanaryan1602 <github1602@gmail.com>
2024-08-19 21:30:05 +0530

    create a middleware func

--- api/index.js
+++ api/index.js
@@ -16,6 +16,16 @@ app.get('/test', (req, res) => {
 app.use("/api/user",userRouter);
 app.use("/api/auth",Authrouter);
 
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.satus(statusCode).json({
+        success : false,
+        statusCode,
+        message
+    })
+})
+
 app.listen(3000, () => {
     console.log('listening on port 3000');
 });
