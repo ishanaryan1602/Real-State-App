f2fcbee146b79efddc3cd1d1f8d2d6955f9a3a0e
ishanaryan1602 <github1602@gmail.com>
2024-08-21 02:21:38 +0530

    Sign In complete

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,12 +8,12 @@ export default function Header() {
         <header className='bg-slate-200 shadow-md'>
             <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
                 <Link to="/">
-                    <h1 className='font-bold text-small sm:text-xl flex flex-wrap '>
-                        <span className='text-slate-500 '>Sahad</span>
-                        <span className='text-slate-700 '>Estate</span>
+                    <h1 className='text-sm sm:text-xl flex flex-wrap'>
+                        <span className='text-slate-500'>The&nbsp;</span>
+                        <span className='text-slate-700 font-semibold'>Estates</span>
                     </h1>
                 </Link>
-                <form className='bg-slate-100 p-3 rounded-lg flex items-center w-24 sm:w-60' >
+                <form className='bg-slate-100 p-3 rounded-lg flex items-center w-32 sm:w-60' >
                     <input type="text" placeholder='Search...' className='bg-transparent outline-none' />
                     <FaSearch className='text-slate-600' />
                 </form>
