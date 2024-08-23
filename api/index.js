7a4d35b3a8bf45bd3a841dbdbdd02745f9e9963e
ishanaryan1602 <github1602@gmail.com>
2024-08-23 14:58:38 +0530

    user update route logic

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import Authrouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 await mongoose.connect(process.env.MONGO).then(() => console.log('connected to db')).catch((err) => console.log(err.message))
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get('/test', (req, res) => {
     res.send('Hello World');
