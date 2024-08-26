11d9488faa404f801372cec543d78e89be0d63bf
ishanaryan1602 <github1602@gmail.com>
2024-08-26 16:35:47 +0530

    create user listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import Listing from "../models/listing.model.js";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 import bcryptjs from "bcryptjs";
@@ -39,8 +40,24 @@ export const deleteUser = async (req, res, next) => {
   }
   try {
     await User.findByIdAndDelete(req.params.id);
-    return res.clearCookie("access_token").status(200).json("User has been deleted successfully");
+    return res
+      .clearCookie("access_token")
+      .status(200)
+      .json("User has been deleted successfully");
   } catch (err) {
     next(err);
+  }
+};
+
+export const getUserListing = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.findById({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (err) {
+      next(err);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own account"));
   }
 };
